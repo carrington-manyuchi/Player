2671f62ca27b995bd72cd869020cc38a2bff1b12
Carrington <Manyuchi>
2023-07-29 07:45:05 +0200

    Initial Commit

---
+++ Player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Player
+//
+//  Created by DA MAC M1 157 on 2023/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
